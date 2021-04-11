9ce96d159429a2b3aaf234e9d8a141f10b208455
boscacci <cinemarob1@gmail.com>
2021-04-11 11:03:06 -0400

    Bug fix: missing semicolon

--- mta_clock/preferences.h
+++ mta_clock/preferences.h
@@ -4,7 +4,7 @@ const char ssid[] = "your-wifi-network-here";
 const char password[] = "your-wifi-password-here";
 
 //Server IP address
-const char serverIP[] = "your-server-ip-here"
+const char serverIP[] = "your-server-ip-here";
 
 // Find the four-character station ID from:
 // https://github.com/boscacci/MTAPI/blob/read-plaintext-stops-transfers/scripts/stations.json
@@ -12,7 +12,7 @@ const char serverIP[] = "your-server-ip-here"
 const char stationID[] = "b2e2";
 
 //Choose prefered train direction, either "N" for Northbound, "S" for Southbound
-#define DIRECTION "N";  //Comment out this line if you've set up a toggle switch instead
+#define DIRECTION "N"; //Comment out this line if you've set up a toggle switch instead
 
 // Set server request interval time in ms
 const unsigned long requestInterval = 15000;
@@ -28,5 +28,5 @@ const byte moreArrivals = 3;
 
 //NTP Server set up for time getting
 const char ntpServer[] = "pool.ntp.org";
-const long  gmtOffset_sec = -18000;  //Time zone in seconds from UTC
-const int   daylightOffset_sec = 3600; //set to 3600 if your country observes DST
+const long gmtOffset_sec = -18000;   //Time zone in seconds from UTC
+const int daylightOffset_sec = 3600; //set to 3600 if your country observes DST
