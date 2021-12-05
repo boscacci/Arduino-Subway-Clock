00b698c458bc950aef06c402300a3a6e7250f327
boscacci <cinemarob1@gmail.com>
2021-12-05 12:57:53 -0500

    Make it daylight savings-aware

--- mta_clock/preferences.h
+++ mta_clock/preferences.h
@@ -1,18 +1,18 @@
 // Enter your Wifi SSID and password here
 // NOTE: ESP32 Does not work with a 5Ghz wifi network only 2.4Ghz
-const char ssid[] = "your-wifi-network-here";
-const char password[] = "your-wifi-password-here";
+const char ssid[] = "YOUR_WIFI_NETWORK_NAME_HERE";
+const char password[] = "YOUR_WIFI_PASSWORD_HERE";
 
-//Server IP address
-const char serverIP[] = "your-server-ip-here";
+// Server IP address
+const char serverIP[] = "YOUR_SERVER_IP_ADDRESS_HERE";
 
 // Find the four-character station ID from:
-// https://github.com/boscacci/MTAPI/blob/read-plaintext-stops-transfers/scripts/stations.json
+// https://github.com/boscacci/MTAPI/blob/master/data/stations.json
 // And enter it here
-const char stationID[] = "b2e2";
+const char stationID[] = "69ba";
 
-//Choose prefered train direction, either "N" for Northbound, "S" for Southbound
-#define DIRECTION "N"; //Comment out this line if you've set up a toggle switch instead
+// If not using a hardware toggle switch, choose prefered train direction, either "N" for Northbound, "S" for Southbound
+//#define DIRECTION "N"; //Comment out this line if you've set up a toggle switch instead
 
 // Set server request interval time in ms
 const unsigned long requestInterval = 15000;
@@ -21,12 +21,10 @@ const unsigned long requestInterval = 15000;
 const unsigned long displayInterval = 3000;
 
 // Filter out arrivals that are less than your time (in minutes) to get to station
-const byte timeToStation = 0;
+const byte timeToStation = 4;
 
 // Set number of additional arrivals to cycle through on the display
 const byte moreArrivals = 3;
 
-//NTP Server set up for time getting
+// NTP Server set up for time getting
 const char ntpServer[] = "pool.ntp.org";
-const long gmtOffset_sec = -18000;   //Time zone in seconds from UTC
-const int daylightOffset_sec = 3600; //set to 3600 if your country observes DST
